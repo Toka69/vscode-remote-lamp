3feca5b010b97ac73833de41c7006bccd6de54f1
Matthias LEROUX <matthiasleroux@laposte.net>
2020-09-30 11:31:55 +0000

    build: add forward port SQL 3306 and modify pma install

--- .devcontainer/scripts/install_pma.sh
+++ .devcontainer/scripts/install_pma.sh
@@ -3,14 +3,17 @@
 APACHE_DOCUMENT_ROOT=workdir
 PHPMYADMIN_VERSION=define_version
 
-#Delete old installation for rebuild
-rm -rf $APACHE_DOCUMENT_ROOT/phpMyAdmin*
-
-#Install PMA
-wget -O /tmp/phpmyadmin.tar.gz https://files.phpmyadmin.net/phpMyAdmin/$PHPMYADMIN_VERSION/phpMyAdmin-$PHPMYADMIN_VERSION-all-languages.tar.gz
-tar xfvz /tmp/phpmyadmin.tar.gz -C $APACHE_DOCUMENT_ROOT
-mv $APACHE_DOCUMENT_ROOT/phpMyAdmin-$PHPMYADMIN_VERSION-all-languages $APACHE_DOCUMENT_ROOT/phpMyAdmin
-mv $APACHE_DOCUMENT_ROOT/phpMyAdmin/config.sample.inc.php $APACHE_DOCUMENT_ROOT/phpMyAdmin/config.inc.php
-sed -ri -e "s/localhost/127.0.0.1/" $APACHE_DOCUMENT_ROOT/phpmyadmin/config.inc.php
+FILE="$APACHE_DOCUMENT_ROOT/phpMyAdmin"
+if [ ! -d "$FILE" ]; then
+    echo "Directory $FILE does not exists -> phpMyAdmin installation"
+    #Install PMA
+    wget -O /tmp/phpmyadmin.tar.gz https://files.phpmyadmin.net/phpMyAdmin/$PHPMYADMIN_VERSION/phpMyAdmin-$PHPMYADMIN_VERSION-all-languages.tar.gz
+    tar xfvz /tmp/phpmyadmin.tar.gz -C $APACHE_DOCUMENT_ROOT
+    mv $APACHE_DOCUMENT_ROOT/phpMyAdmin-$PHPMYADMIN_VERSION-all-languages $APACHE_DOCUMENT_ROOT/phpMyAdmin
+    mv $APACHE_DOCUMENT_ROOT/phpMyAdmin/config.sample.inc.php $APACHE_DOCUMENT_ROOT/phpMyAdmin/config.inc.php
+    sed -ri -e "s/localhost/127.0.0.1/" $APACHE_DOCUMENT_ROOT/phpmyadmin/config.inc.php
+else
+    echo "Directory $FILE exists."
+fi
 
 sudo chown -R www-data $APACHE_DOCUMENT_ROOT/phpMyAdmin
